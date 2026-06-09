cca173210ec364b63b1f2f198b15dcbb1833eac4
Teekay-OG <Talibsanusi02@gmail.com>
2026-06-09 15:26:21 +0100

    Fix CartContext casing for Netlify

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 
-import CartContextProvider from './context/CartContext'
+import CartContextProvider from './context/CartContext.jsx'
 import WishlistContextProvider from './context/WishlistContext'
 
 createRoot(document.getElementById('root')).render(
